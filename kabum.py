d0bb5a0854daf781f5bbf91f277b0506bad697cf
Fr0sne <gabriel16david@hotmail.com>
2021-09-11 00:40:02 -0300

    Update kabum.py

--- kabum.py
+++ kabum.py
@@ -1,9 +1,11 @@
+from os import path, read
 import socket
 import sys
 import json
+from os import path
 
 
-def desligar():
+def desligar(socket):
     print("[-]Desligando...")
     b1 = b"\x28\x6d\xcd\x42\x07\x55\x08\x00\x27\x8d\x68\x4f\x08\x00\x45\x00" \
     b"\x00\x6f\x95\xef\x40\x00\x81\x06\xe2\x34\xc0\xa8\x00\x12\xc0\xa8" \
@@ -14,13 +16,13 @@ def desligar():
     b"\x1b\xa5\xf1\x18\x47\x52\xe4\x3f\xc4\xff\xb2\x2e\x8f\xeb\x2d\xd8" \
     b"\x7e\x91\xcc\xa3\xb8\x44\x4c\x49\xb3\x00\x00\xaa\x55"
     
-    s.send(b1)
-    s.close()
+    socket.send(b1)
+    socket.close()
 
 
 
 
-def ligar():
+def ligar(socket):
     print("[+]Ligando...")
     b1 = b"\x28\x6d\xcd\x42\x07\x55\x08\x00\x27\x8d\x68\x4f\x08\x00\x45\x00" \
     b"\x00\x6f\x96\xec\x40\x00\x81\x06\xe1\x37\xc0\xa8\x00\x12\xc0\xa8" \
@@ -30,64 +32,53 @@ def ligar():
     b"\x9a\x00\x05\x78\xec\x4f\x97\x2f\x50\xdf\x0f\x23\x64\xed\xfb\xad" \
     b"\xf7\x27\x30\x4d\x19\x20\x51\x73\xf1\x05\x52\x0d\x28\x40\xc0\x79" \
     b"\x86\xfb\xdc\xba\xab\x9c\xe9\xec\x67\x00\x00\xaa\x55"
-    s.send(b1)
-    s.close()
+    socket.send(b1)
+    socket.close()
+def errmsg():
+    print("Por favor, use: " + __file__.split("\\")[-1] + " [on|off]")
+def getIp(ipTail):
+    ipTail = str(ipTail)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hostname = socket.gethostname()
+    iplocal = ""
+    if len(ipTail) > 3:
+        iplocal = ipTail
+    else:
+        iplocal = socket.gethostbyname_ex(hostname)[2][1]
+        iplocal = ".".join(iplocal.split(".")[0:3]) + "." + str(ipTail)
+    s.settimeout(0.5)
+    print(iplocal)
+    r = s.connect_ex((iplocal, port))
+    if r == 0:
+        try:
+            if sys.argv[1] == 'on': ligar(s)
+            elif sys.argv[1] == 'off': desligar(s)
+            else : errmsg()
+        except:
+                errmsg()
+                pass
+
+        return iplocal
+    else:
+        return False
+    
 
 port = 6668
-with open("C:\\Users\\Frosne\\Desktop\\kabum.json", "r+") as f:
-    data = json.loads(f.read())
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ip = data['ip']
-    s.settimeout(1)
-    print("Tentando conectar em: " + ip)
-    connected = s.connect_ex((ip, port))
-    if connected == 0:
-        if data['on']:
-            data['on'] = False
-            f.seek(0)
-            f.truncate()
-            f.write(json.dumps(data, indent=4))
-            f.close()
-            desligar()
-        else:
-            data['on'] = True
-            f.seek(0)
-            f.truncate()            
-            f.write(json.dumps(data, indent=4))
-            ligar()
-    else:
-        for x in range(30):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            ip = "192.168.0." + str(x)
-            print("Tentando conectar em: " + ip)
-            s.settimeout(1)
-            connected = s.connect_ex((ip, port))
-            if connected == 0:
-                with open("C:\\Users\\Frosne\\Desktop\\kabum.json", 'r+') as f:
-                    data = json.loads(f.read())
-                    if data['on']:
-                        data['ip'] = ip
-                        data['on'] = False
-                        f.seek(0)
-                        f.truncate()
-                        f.write(json.dumps(data, indent=4))
-                        f.close()
-                        desligar()
-                    else:
-                        data['ip'] = ip
-                        data['on'] = True
-                        f.seek(0)
-                        f.truncate()
-                        f.write(json.dumps(data, indent=4))
-                        f.close()
-                        ligar()
-                break
+o, i, n = (None,)*3
+try:
+    o = open('kbmip.txt', 'r')
+    i = o.read()
+    n = i if getIp(i) else False
+except:
+    pass
 
 
-
-
-# if sys.argv[1] == "on":
-#     ligar()
-# elif sys.argv[1] == "off":
-#      desligar()
-# print(b1 + "\n" + b2 + "\n" + b3)
+if not n:
+    for x in range(2,255):
+        n = getIp(x)
+        if n != False:
+            print("Endereço da Lâmpada: " + n + ":" + str(port))
+            with open('kbmip.txt', 'w+') as f:
+                f.write(n)
+                
+            break
